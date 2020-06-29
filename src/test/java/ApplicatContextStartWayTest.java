047b2c0d1842e9ad39270a7d6055f65dacaadfa8
MaxHDN <414283143@qq.com>
2020-06-29 20:55:43 +0800

    第二次提交

--- src/test/java/ApplicatContextStartWayTest.java
+++ src/test/java/ApplicatContextStartWayTest.java
@@ -8,7 +8,13 @@ public class ApplicatContextStartWayTest {
 
     @Test
     public void iocTest() throws Exception {
+
+        // 通过读取classpath路径下配置文件，来启动容器。
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+        // 通过读取本地磁盘配置文件，来启动容器。不推荐
+        //  ApplicationContext applicationContext = new FileSystemXmlApplicationContext("文件系统的绝对路径");
+
         AccountDao accountDao = (AccountDao) applicationContext.getBean("accountDao");
         Account account = accountDao.queryAccountByCardNo("6029621011000");
         System.out.println(account);
